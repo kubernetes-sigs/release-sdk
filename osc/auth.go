31d816212e82a08841575a2ccc6c0a9c2f9a7435
Marko Mudrinić <mudrinic.mare@gmail.com>
2023-05-25 18:48:59 +0200

    Add CreateOSCConfigFile function
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

---
+++ osc/auth.go
@@ -0,0 +1,48 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package osc
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	authFileFormat = `
+[general]
+apiurl = %s
+
+[%s]
+user=%s
+pass=%s
+credentials_mgr_class=osc.credentials.PlaintextConfigFileCredentialsManager
+`
+
+	oscConfigFilePath = "/root/.oscrc"
+)
+
+// CreateOSCConfigFile creates the osc config file (~/.oscrc) that contains
+// API URL and credentials needed to authenticate with the API
+func CreateOSCConfigFile(apiURL, username, password string) error {
+	authFile := fmt.Sprintf(authFileFormat, apiURL, apiURL, username, password)
+
+	if err := os.WriteFile(oscConfigFilePath, []byte(authFile), 0600); err != nil {
+		return fmt.Errorf("writing osc config file: %w", err)
+	}
+
+	return nil
+}
